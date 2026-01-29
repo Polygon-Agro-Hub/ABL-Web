2053e6a604bb74b0f48662d8b4be8261d065fad3
Pasan-FDo <pasanfernando.work@gmail.com>
2026-01-29 16:11:14 +0530

    Add NoCareerData component and update CareerPage to handle no data scenario

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -1,11 +1,16 @@
 import Career from "@/components/career/Career";
 import CareerData from "@/components/career/CareerData";
+import NoCareerData from "@/components/career/NoCareerData";
 
 export default function CareerPage() {
   return (
     <div className="flex flex-col my-18 mx-auto lg:gap-20">
-      <Career />
-      <CareerData />
+      {/* If career data is available, use the following components */}
+      {/* <Career /> */}
+      {/* <CareerData /> */}
+
+      {/* If no career data is available, use the following component */}
+      <NoCareerData />
     </div>
   );
 }
---
+++ components/career/NoCareerData.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const NoCareerData = () => {
+    return (
+       <section>
+      <div className="mx-auto text-center">
+        {/* Title */}
+        <div className="bg-[#F6F8FD] py-16 px-6">
+          <h1 className="text-4xl text-black font-medium">
+            Be part of our mission
+          </h1>
+          <div className="max-w-7xl mx-auto text-black ">
+            {/* Introduction */}
+            <p className="text-lg mt-6">
+             No current openings, but we’re growing fast. Stay tuned!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    )
+}
+
+export default NoCareerData
